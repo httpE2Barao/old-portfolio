53f061d8623ac0b706696d295526e24d5d937706
Elias E. Barão <90708733+httpE2Barao@users.noreply.github.com>
2023-03-14 15:32:09 -0300

    Add files via upload

---
+++ app/animation.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', () => {
+    const element = document.querySelectorAll('.animate');
+    element.forEach( (element) => {
+        const isInView = element.getBoundingClientRect().top <= window.innerHeight / 2;
+        element.classList.remove('show');
+        if (isInView) {
+          element.classList.add('show');
+        } 
+    })
+});
