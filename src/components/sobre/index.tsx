a766bf3402298f0ac79e914693ef7c523430cda6
httpE2Barao <elias.barao@hotmail.com>
2023-06-12 16:16:41 -0300

    Ajustes no CSS

--- src/components/sobre/index.tsx
+++ src/components/sobre/index.tsx
@@ -1,30 +1,28 @@
 import React from "react"
 
 const Sobre = () => {
-    return (
-        <article id="pag1" className="animate">
-        <a href="#pag1">
-          <h1 className="title">About-me</h1>
-          <div className="gradientLine"></div>
-        </a>
-  
-        <section className="sobreMim">
-          <div className="resumo">
-            <p>
-              Olá! Eu sou uma pessoa autodidata, artística e criativa, que gosta de se expressar e se comunicar. 
-              Sou um desenvolvedor Front-End apaixonado por transformar ideias em produtos reais. Fiz um curso a parte 
-              de audiovisual e tenho experiência com conceitos de design (UI/UX), comunicação e relações interpessoais. 
-              Além disso, estou sempre buscando aprender e me aprimorar cada vez mais no universo do desenvolvimento web, 
-              usando ferramentas modernas como React, Javascript e Typescript. Com código limpo e eficiente, eu tenho trabalhado 
-              na construção de aplicações acessíveis e responsivas. Fora do mundo da programação, sou um ávido gamer e tenho grande 
-              interesse nas Artes e Espiritualidade. Meu objetivo como desenvolvedor é criar experiências online inovadoras e 
-              acessíveis para usuários em todo o mundo.
-            </p>
-            <label htmlFor="bt">Download CV</label>
-            <a href="/cv-eliasbarao.pdf" download className="bt">Download CV</a>
-          </div>
-        </section>
-      </article>
-    )
+  return (
+    <article id="pag1" className="animate">
+      <a href="#pag1">
+        <h1 className="title">About-me</h1>
+        <div className="gradientLine"></div>
+      </a>
+
+      <section className="sobreMim">
+        <div className="resumo">
+          <p>
+            Sou um desenvolvedor Front-End que adora se expressar e transformar ideias em realidade.
+            Tenho formação em audiovisual e experiência com design (UI/UX), comunicação e relações interpessoais.
+            Uso ferramentas modernas como React, Javascript e Typescript para criar aplicações web acessíveis e responsivas,
+            com código limpo e eficiente. Além de ser autodidata, artístico e criativo, também sou um gamer entusiasta e
+            tenho interesse nas Artes e Espiritualidade. Meu objetivo é criar experiências online inovadoras e acessíveis
+            para usuários em todo o mundo.
+          </p>
+          <label htmlFor="bt">Download CV</label>
+          <a href="/cv-eliasbarao.pdf" download className="bt">Download CV</a>
+        </div>
+      </section>
+    </article>
+  )
 }
 export default Sobre
