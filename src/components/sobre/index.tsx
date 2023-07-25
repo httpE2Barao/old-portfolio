de88480586d0ba02b6d340c07a81dffe96fa7274
httpE2Barao <elias.barao@hotmail.com>
2023-07-25 16:53:29 -0300

    atualizando texto de apresentação

--- src/components/sobre/index.tsx
+++ src/components/sobre/index.tsx
@@ -11,12 +11,13 @@ const Sobre = () => {
       <section className="sobreMim">
         <div className="resumo">
           <p>
-            Sou um desenvolvedor Front-End que adora se expressar e transformar ideias em realidade.
-            Tenho formação em audiovisual e experiência com design (UI/UX), comunicação e relações interpessoais.
-            Uso ferramentas modernas como React, Javascript, Typescript, Bootstrap, Tailwind... para criar aplicações web acessíveis e responsivas,
-            com código limpo e eficiente. Além de ser autodidata, artístico e criativo, também sou um gamer entusiasta e
-            tenho interesse nas Artes e Espiritualidade. Meu objetivo é criar experiências online inovadoras e acessíveis
-            para usuários em todo o mundo.
+          Como desenvolvedor Front-End, minha paixão é materializar ideias através da combinação entre código e criatividade. 
+          Minha formação em audiovisual e experiência em design (UI/UX), comunicação e habilidades interpessoais me permitem abordar projetos com uma visão holística. 
+          Com habilidades em React, Javascript, Typescript, Bootstrap, Tailwind e JQuery, crio aplicações web que primam pela acessibilidade, responsividade e eficiência.
+
+          Minha busca constante pelo conhecimento como autodidata enriquece minha abordagem artística e criativa. 
+          Como entusiasta gamer e amante das artes e espiritualidade, busco inspiração em diversas fontes para oferecer experiências online inovadoras e verdadeiramente envolventes para usuários de todas as partes do mundo. 
+          Meu objetivo é transcender as expectativas e proporcionar aos usuários uma jornada digital única e marcante.
           </p>
           <label htmlFor="bt">Download CV</label>
           <a href="/cv_minimalista-eliasbarao.pdf" download className="bt">Download CV</a>
